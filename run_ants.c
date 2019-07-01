cb4b1a6345a6effff04845ac7f664d3c95d315f4
Tetiana BONDARENKO <tbondare@e1r9p2.unit.ua>
2019-07-01 20:12:22 +0300

    run_ants

--- run_ants.c
+++ run_ants.c
@@ -1,9 +1,55 @@
 #include "libftlemin.h"
 #include "stdio.h"
 
+char *write_float_in_str(float x)
+{
+    int x_int;
+    char *str;
+    char *str2;
+
+    if (x < 1)
+    {
+        str = (char*)malloc(sizeof(char) * 3);
+        str[0] = '0';
+        str[1] = ',';
+        str[2] = '\0';
+    }
+    else
+    {
+        x_int = x;
+        str = ft_itoa(x_int);
+        str2 = (char*)malloc(sizeof(char) * 2);
+        str2[0] = ',';
+        str2[1] = '\0';
+        str = ft_strjoin(str, str2);
+        free(str2);
+        x_int = x;
+        x = x - x_int;
+    }
+    while (x)
+    {
+        x_int = x;
+        x = x - x_int;
+        x = x * 10;
+        str2 = (char*)malloc(sizeof(char) * 2);
+        str2[0] = x_int + '0';
+        str2[1] = '\0';
+        str = ft_strjoin(str, str2);
+        free(str2);
+    }
+    return (str);
+}
+
 int ft_round(float x)
 {
-    
+    int i;
+    int cnt_dgt;
+    char *arr_dgt;
+
+    i = 0;
+    if (x == 0)
+        return (0);
+    write_float_in_str(x);
     return (x);
 }
 
@@ -12,7 +58,6 @@ void cnt_x(t_input_data *data, t_lst_vld_path *lst_vld_path)
     int n;
     int prdct_b;
     int sum_y;
-    float x_
     t_lst_vld_path *crnt;
 
     n = 0;
