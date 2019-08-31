62ae46ed5184476f13a79e2ecd1cf28450e34bfd
Tetiana BONDARENKO <tbondare@e1r1p3.unit.ua>
2019-08-31 16:27:48 +0300

    forgot *h

--- libftlemin.h
+++ libftlemin.h
@@ -1,109 +1,132 @@
-//
-// Created by tanya on 15.09.18.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftlemin.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbondare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/23 16:04:18 by tbondare          #+#    #+#             */
+/*   Updated: 2019/08/30 20:29:02 by tbondare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef LEM_IN_LIBFTLEMIN_H
-#define LEM_IN_LIBFTLEMIN_H
+# define LEM_IN_LIBFTLEMIN_H
 
-# include"libft/libft.h"
+# include "libft/libft.h"
 # include <stdlib.h>
 
 struct s_links;
 struct s_ls_come_from;
 
-typedef struct s_list_rooms
+typedef struct				s_list_rooms
 {
-    char *name;
-    int x;
-    int y;
-    int is_sart;
-    int is_end;
-    int was_in_room;
-    int is_in_queue;
-    int is_in_val_pth;
-    struct s_links *link;
-    struct s_ls_come_from *from;
-    struct s_list_rooms *next_rm;
-}t_list_rooms;
+	char					*name;
+	int						x;
+	int						y;
+	int						is_sart;
+	int						is_end;
+	int						was_in_room;
+	int						is_in_queue;
+	int						is_in_val_pth;
+	struct s_links			*link;
+	struct s_ls_come_from	*from;
+	struct s_list_rooms		*next_rm;
+}							t_list_rooms;
 
-typedef struct s_ls_come_from
+typedef struct				s_ls_come_from
 {
-    struct s_list_rooms *room;
-    struct s_ls_come_from *next;
-}t_ls_come_from;
+	struct s_list_rooms		*room;
+	struct s_ls_come_from	*next;
+}							t_ls_come_from;
 
-typedef struct s_links
+typedef struct				s_links
 {
-    t_list_rooms *linked_room;
-    struct s_links *next;
-}t_links;
+	t_list_rooms			*linked_room;
+	struct s_links			*next;
+}							t_links;
 
-typedef struct s_list_tubes
+typedef struct				s_list_tubes
 {
-    char *first;
-    char *second;
-    struct s_list_tubes *next_tb;
-}t_list_tubes;
+	char					*first;
+	char					*second;
+	struct s_list_tubes		*next_tb;
+}							t_list_tubes;
 
-typedef struct s_input_data
+typedef struct				s_input_data
 {
-    int flg_ants;
-    int cnt_ants;
-    char *end_room_name;
-    t_list_rooms *frst_rm;
-    t_list_tubes *frst_tb;
+	int						flg_ants;
+	int						cnt_ants;
+	char					*end_room_name;
+	t_list_rooms			*frst_rm;
+	t_list_tubes			*frst_tb;
+}							t_input_data;
 
-}t_input_data;
-
-typedef struct s_vld_path_elem
+typedef struct				s_vld_path_elem
 {
-   t_list_rooms *room;
-   struct s_vld_path_elem *next;
-}t_vld_path_elem;
+	t_list_rooms			*room;
+	struct s_vld_path_elem	*next;
+}							t_vld_path_elem;
 
-typedef struct s_lst_vld_path
+typedef struct				s_lst_vld_path
 {
-    int leng;
-    int y;
-    int x;
-    t_vld_path_elem *frst_path_el;
-    struct s_lst_vld_path *next;
-}t_lst_vld_path;
+	int						leng;
+	int						y;
+	int						x;
+	t_vld_path_elem			*frst_path_el;
+	struct s_lst_vld_path	*next;
+}							t_lst_vld_path;
 
-typedef struct s_list_queue
+typedef struct				s_list_queue
 {
-    t_list_rooms *crn_room;
-    struct s_list_queue *next;
-}t_list_queue;
+	t_list_rooms			*crn_room;
+	struct s_list_queue		*next;
+}							t_list_queue;
 
-typedef struct s_queue_data
+typedef struct				s_queue_data
 {
-    t_list_queue *frst_queue_el;
-    t_list_queue *mem_queue;
-    t_list_queue *last_q_el;
-}t_queue_data;
+	t_list_queue			*frst_queue_el;
+	t_list_queue			*mem_queue;
+	t_list_queue			*last_q_el;
+}							t_queue_data;
 
-typedef struct s_out_room
+typedef struct				s_out_room
 {
-    char *name;
-    int ant_index;
-}t_out_room;
+	char					*name;
+	int						ant_index;
+}							t_out_room;
 
-typedef struct s_out_path
+typedef struct				s_out_path
 {
-    int nmb_ants;
-    int path_leng;
-    t_out_room *path;
-}t_out_path;
+	int						nmb_ants;
+	int						path_leng;
+	t_out_room				*path;
+}							t_out_path;
 
-typedef struct s_output_data
+typedef struct				s_output_data
 {
-    int nmb_paths;
-    t_out_path *arr_paths;
-}t_output_data;
+	int						nmb_paths;
+	t_out_path				*arr_paths;
+}							t_output_data;
 
-int ft_read_data(t_input_data *data);
-t_lst_vld_path *find_valid_path(t_input_data *data);
-void run_ants(t_input_data *data, t_lst_vld_path *lst_vld_path);
+t_list_rooms				*find_start_room(t_input_data *data);
+void						add_queue_el(t_queue_data *que, t_list_rooms *el_que);
+void						del_first_queue_el(t_queue_data *queue);
+void						add_come_from_room(t_list_rooms *come_from_rm, t_list_rooms *crnt_rm);
+void						del_queue(t_queue_data *queue);
 
-#endif //LEM_IN_LIBFTLEMIN_H
+void						whl_f_save_vl_path(t_list_rooms *crn_room);
+void						save_val_path(t_lst_vld_path **frst_vl_pth, t_list_rooms *crn_room, t_input_data *data);
+void						els_f_whl_fnd_vl_path(t_links *crn_rm_ln, t_queue_data **queue);
+void						whl_f_find_val_path(t_input_data *data, t_links *crn_rm_ln, t_queue_data *queue, t_lst_vld_path **frst_vl_pth);
+t_lst_vld_path				*find_valid_path(t_input_data *data);
+
+void del_el_val_path(t_vld_path_elem *frs_el);
+void del_rm_lnks(t_links *rm_lnk);
+void del_rm_from(t_ls_come_from *from);
+void free_mem(t_input_data *data);
+void free_mem_val_path(t_lst_vld_path *lst_vld_path);
+
+
+
+#endif
